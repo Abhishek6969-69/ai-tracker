e35215e1913ac79e717c32a6cb2708fd9b7761cb
Abhishek6969-69 <sbh123yadav@gmail.com>
2025-12-18 03:39:42 +0530

    prisma error and build eror resolved

--- src/lib/get-session-user.ts
+++ src/lib/get-session-user.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth-options";
 
 export async function getSessionUser() {
+  const { authOptions } = await import("@/lib/auth-options");
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) {
